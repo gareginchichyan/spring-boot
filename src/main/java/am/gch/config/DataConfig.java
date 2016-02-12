ddea2b7977c83f7d0f7c1f7894ae1f299dff51b5
Garegin Chichyan <gareginchichyan@workfront.com>
2016-02-13 02:10:00 +0400

    minor changes

--- src/main/java/am/gch/config/DataConfig.java
+++ src/main/java/am/gch/config/DataConfig.java
@@ -1,10 +1,26 @@
 package am.gch.config;
 
+import am.gch.domain.*;
+import org.springframework.beans.factory.annotation.*;
 import org.springframework.context.annotation.*;
+import org.springframework.core.env.*;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.*;
 
+//@Profile("default") // @Profile({"p1", "!p2"}), p1 is active, or p2 is not active
 @Configuration
 @EnableTransactionManagement
 public class DataConfig {
+
+	@Autowired
+	private MvcConfig mvcConfig;
+
+	@Autowired
+	private Environment env;
+
+	@Bean
+	public User getUser() {
+		User user = new User();
+		return user;
+	}
 }
